6724c44331cf5d166aaeec52130e8273e1b92b40
CHETAN MANOHAR PATIL <cmpatil6648@gmail.com>
2024-06-17 10:41:11 +0530

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+#band name generator
+print("Welcome to the Band name generator.! ")
+city= input ("Name of the City you grew up? \n")
+pet = input("Name of your pet? \n")
+print("The name of your Band Could be "+ city +" "+ pet)
